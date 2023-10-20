eed294ed00611dc7ed6c538faf2058948e3ff795
wcbup <wcbup123@foxmail.com>
2023-10-20 21:34:34 +0200

    restruct the dr set

--- HeuristicMiner.py
+++ HeuristicMiner.py
@@ -50,39 +50,39 @@ class DcCountingSet:
             del self.counting_dict[id]
 
 
-class DrTuple:
-    def __init__(self, task_relation: Tuple[str, str], time_delta: int) -> None:
-        self.task_relation = task_relation
-        self.frequency = 1
-        self.time_delta = time_delta
-
-
 class DrCountingSet:
     def __init__(self) -> None:
-        self.counting_dict: Dict[Tuple[str, str], DrTuple] = {}
+        # the first item in the tuple is frequency
+        # the second item in the tuple is delta
+        self.counting_dict: Dict[str, Dict[str, Tuple[int, int]]] = {}
 
-    def update(self, task_relation: Tuple[str, str], bucket_id: int) -> bool:
+    def update(self, pred_task: str, succ_task: str, bucket_id: int) -> bool:
         """
         if relation exists, update the frequency automatically
         if not, add new tuple automatically
         """
-        if task_relation in self.counting_dict.keys():
-            self.counting_dict[task_relation].frequency += 1
-            return True
+        if pred_task in self.counting_dict.keys():
+            tmp_dict = self.counting_dict[pred_task]
+            if succ_task not in tmp_dict:
+                tmp_dict[succ_task] = (1, bucket_id - 1)
+            else:
+                old_tuple = tmp_dict[succ_task]
+                tmp_dict[succ_task] = (old_tuple[0] + 1, old_tuple[1])
         else:
-            self.counting_dict[task_relation] = DrTuple(task_relation, bucket_id - 1)
-            return False
+            self.counting_dict[pred_task] = {succ_task: (1, bucket_id - 1)}
 
     def cleanup(self, bucket_id: int) -> None:
         del_list = []
 
-        for relation in self.counting_dict:
-            dr_tuple = self.counting_dict[relation]
-            if dr_tuple.frequency + dr_tuple.time_delta <= bucket_id:
-                del_list.append(relation)
+        for pred_task in self.counting_dict.keys():
+            tmp_dict = self.counting_dict[pred_task]
+            for succ_task in tmp_dict.keys():
+                tmp_tuple = tmp_dict[succ_task]
+                if tmp_tuple[0] + tmp_tuple[1] <= bucket_id:
+                    del_list.append((pred_task, succ_task))
 
-        for id in del_list:
-            del self.counting_dict[id]
+        for pred_task, succ_task in del_list:
+            del self.counting_dict[pred_task][succ_task]
 
 
 class HeuristicMiner:
@@ -103,14 +103,38 @@ class HeuristicMiner:
         # update counting set
         last_task_name = self.dc_set.update_case(new_case_id, new_task_name, bucket_id)
         if last_task_name != None:
-            self.dr_set.update((last_task_name, new_task_name), bucket_id)
+            self.dr_set.update(last_task_name, new_task_name, bucket_id)
 
         # cleanup
         if self.counter % self.bucket_size == 0:
+            # print("before cleanup")
+            # self.print_set()
+
             self.dc_set.cleanup(bucket_id)
             self.dr_set.cleanup(bucket_id)
-        
+
+            # print("after cleanup")
+            # self.print_set()
+
         self.counter += 1
+    
+    def print_set(self) -> None:
+        # print("---dc---")
+        # for case_id in self.dc_set.counting_dict.keys():
+        #     dc_tuple = self.dc_set.counting_dict[case_id]
+        #     print(case_id)
+        #     print(f" {dc_tuple.task_name} f:{dc_tuple.frequency} {dc_tuple.time_delta}")
+        #     print()
+        # print()
+        print("---dr---")
+        for pred_task in self.dr_set.counting_dict.keys():
+            tmp_dict = self.dr_set.counting_dict[pred_task]
+            print(pred_task)
+            for succ_task in tmp_dict.keys():
+                tmp_tuple = tmp_dict[succ_task]
+                print(f" {succ_task} f:{tmp_tuple[0]} {tmp_tuple[1]}")
+                print()
+    
 
 
 # test code
@@ -118,8 +142,15 @@ if __name__ == "__main__":
     b_events = log_source("extension-log-4.xes")
     # b_events = b_events.pipe(operators.take(5))
 
-    miner = HeuristicMiner(0.000000000001, 0.8)
+    miner = HeuristicMiner(0.0005, 0.8)
     b_events.subscribe(lambda x: miner.get_new_event(x))
 
-    # for relation in miner.dr_set.counting_dict.keys():
-    #     print(relation, miner.dr_set.counting_dict[relation].frequency)
+    # for pred_task in miner.dr_set.counting_dict.keys():
+    #     tmp_dict = miner.dr_set.counting_dict[pred_task]
+    #     print(pred_task)
+    #     for succ_task in tmp_dict:
+    #         print(f" {succ_task} {tmp_dict[succ_task]}")
+    #     print()
+
+    miner.print_set()
+
